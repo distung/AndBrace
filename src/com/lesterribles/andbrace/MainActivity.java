5fef4ba3a34505ee292077579ce826a16b492865
The Grimace <Nightvoomer@gmail.com>
2012-08-21 18:48:00 -0500

    made a default strategy

--- src/com/lesterribles/andbrace/MainActivity.java
+++ src/com/lesterribles/andbrace/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NavUtils;
 
@@ -12,6 +13,12 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        TableLayout table = new TableLayout(this);  
+        
+        table.setStretchAllColumns(true);  
+        table.setShrinkAllColumns(true);
+        
         setContentView(R.layout.activity_main);
     }
 

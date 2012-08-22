462584d5f8047dc466ab295dd8bea1bf73d49942
The Grimace <Nightvoomer@gmail.com>
2012-08-21 19:18:54 -0500

    default seeding strategy and bye player

---
+++ src/com/tournament/DefaultSeedingStrategy.java
@@ -0,0 +1,20 @@
+package com.tournament;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DefaultSeedingStrategy implements ISeedingStrategy {
+
+	@Override
+	public ArrayList<Player> Seed(ArrayList<Player> players) {
+		int tournamentSize = (int) Math.pow(2, Math.ceil((Math.log(players.size()) / Math.log(2))));
+		int numByes = tournamentSize - players.size();
+		for (int i = 0; i < numByes; i++) {
+			ByePlayer bye = new ByePlayer();
+			players.add(bye);
+		}
+		Collections.shuffle(players);
+		return players;
+	}
+
+}

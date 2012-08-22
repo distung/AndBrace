778cb26b5be142ae03db8ac40026d5a9dfad6494
Tung Le <distung@gmail.com>
2012-08-21 19:25:02 -0500

    No real change.

--- src/com/lesterribles/andbrace/MainActivity.java
+++ src/com/lesterribles/andbrace/MainActivity.java
@@ -9,7 +9,6 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;

413ad3477893ffc62ff6949e9f9326ba67453ebe
The Grimace <Nightvoomer@gmail.com>
2012-09-14 19:51:29 -0500

    Added Constructors and fields to Player and ByePlayer

--- src/com/tournament/ByePlayer.java
+++ src/com/tournament/ByePlayer.java
@@ -1,6 +1,11 @@
 package com.tournament;
 
 public class ByePlayer extends Player {
+	public ByePlayer(String firstName, String lastName, String handle) {
+		super(firstName, lastName, handle);
+		// TODO Auto-generated constructor stub
+	}
+
 	public boolean isByePlayer() {
 		return true;
 	}
--- src/com/tournament/Player.java
+++ src/com/tournament/Player.java
@@ -1,6 +1,19 @@
 package com.tournament;
 
 public class Player {
+	String FirstName;
+	String LastName;
+	String Handle;
+	public Player() {
+		
+	}
+	
+	public  Player(String firstName, String lastName, String handle) {
+		FirstName = firstName;
+		LastName = lastName;
+		Handle = handle;
+	}
+	
 	public boolean isByePlayer() {
 		return false;
 	}
